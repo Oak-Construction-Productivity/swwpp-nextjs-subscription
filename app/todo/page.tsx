7d0665a82f9a6977f8021c9d94b16d35a545f837
Ira <iraf333@gmail.com>
2023-09-25 21:18:56 -0400

    add todo list items for key item tasks

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -16,15 +16,43 @@ export default async function Todo() {
         <div className="flex flex-col justify-center pb-12 ">
   <div className="flex items-center mb-2 p-2 border rounded">
     <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
-    <div>Item 1</div>
+    <div>Add report automation scheduling into database column (frequency)</div>
   </div>
   <div className="flex items-center mb-2 p-2 border rounded">
     <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
-    <div>Item 2</div>
+    <div>Create a form that successfully updates the project database</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Display projects on the project page</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Form handeling and not letting non payers add more than one/two projects</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Create a button/utility function that successfully deletes the project, in project settings</div>
   </div>
   <div className="flex items-center p-2 border rounded">
     <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
-    <div>Item 3</div>
+    <div>Slug project page, integrate a weather api to display 10 day percipitation schedule and notable info</div>
+  </div>
+  <div className="flex items-center p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>In project page, allow for info to be added then a button to send email report</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Create a button/utility function that successfully deletes the project, in project settings</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Improve the homepage and design</div>
+  </div>
+  <div className="flex items-center mb-2 p-2 border rounded">
+    <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+    <div>Figure out report automation with stmp or something</div>
   </div>
 </div>
       </div>
