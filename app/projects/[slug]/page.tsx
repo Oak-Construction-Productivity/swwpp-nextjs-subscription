96539a525649e27af759cf268575366354c2b4a0
Ira <iraf333@gmail.com>
2023-09-27 15:39:02 -0400

    add dynamic slug rouTING FOR PROJECTS

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,13 @@
+export default function Page({ params }: { params: { slug: string } }) {
+    return <div>My Post: {params.slug}</div>
+  }
+
+  //pull user.id
+  //make sure it matches the uuid before rendering
+  //pull in project data
+  //pull in rain and weather data
+  //create dashboard
+  //create google map
+  //edit information form button
+  //reporting status
+  
