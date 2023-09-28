3fb37cf52f79a83e9fe006e53e031f78f4b0cbf8
Ira <iraf333@gmail.com>
2023-09-28 14:53:19 -0400

    updates

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,20 @@
+'use client'
 import { useEffect, useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
+const supabase = createClientComponentClient();
+
+type UserType = string | null;
+type ProjectType = any;
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const [user, setUser] = useState<UserType>(null)
+  const [error, setError] = useState<UserType>(null);
+  const [weatherData, setWeatherData] = useState<UserType>(null);
+  const [long, setLong] = useState<UserType>(null);
+  const [lat, setLat] = useState<UserType>(null);
+  const [projects, setProjects] = useState<ProjectType[]>([]);
+
   function getPreviousDayDate(): string {
     const today = new Date();
     const yesterday = new Date(today);
@@ -13,17 +27,71 @@ export default function Page({ params }: { params: { slug: string } }) {
     return `${year}-${month}-${day}`;
   }
   const previousDayDate = getPreviousDayDate();
-  console.log(previousDayDate); // Example output: "2023-09-27"
 
-  useEffect(() => {}, []);
+  //useEffect to retrieve the user info, make sure it matches the user.id of the project number
+  // return the project information to render, pass the latitude coordinates to the weather api function
+  useEffect(() => {
+    const afunctiontocall = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data) {
+        console.log(data, data?.session?.user);
+        if (data?.session?.user) {
+          setUser(data?.session?.user?.id)
+          //setLat()
+          //setLong()
+        }
+      } else {
+        console.log('false ities');
+      }
+    };
+    afunctiontocall();
+  }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        let { data: projects, error } = await supabase
+          .from('projects')
+          .select('*')
+          .eq('id', params.slug);
+
+        if (error) {
+          setError(error.message);
+        } else {
+          setProjects(projects || [] || null);
+          console.log(projects);
+          console.log(error);
+        }
+      } catch (error: any) {
+        setError(error.message);
+      }
+    }
+    console.log(projects, 'data displayed here');
+    fetchProjects();
+  }, []);
+
+/*
+  useEffect(() => {
+    // Construct the URL for the API call
+    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${date}&tz=${tz}&appid=${apiKey}`;
+
+    // Make the API call using the fetch API
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+      });
+  }, [long]); */
 
   return (
     <>
       <div>My Post: {params.slug}</div>
       <div>Yesterdays Date: {previousDayDate}</div>
       <div>Yesterdays weather: {previousDayDate}</div>
+      <div>My Id: {user}</div>
     </>
   );
 }
