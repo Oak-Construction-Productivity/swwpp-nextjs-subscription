b3b7679fd0e222f591ccad528d505281af7ace05
Ira <iraf333@gmail.com>
2023-09-28 15:55:09 -0400

    add main weather data api to the project slug page, weather is pulled in rendering correctly

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,8 +1,18 @@
 'use client'
 import { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 const supabase = createClientComponentClient();
+const supabaseUrl = 'https://eggvjyydqfibdrgfyyny.supabase.co';
+const supabaseKey: string | undefined =
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
+if (!supabaseKey) {
+  throw new Error(
+    'SUPABASE_SERVICE_ROLE_KEY is not set in the environment variables.'
+  );
+}
+const superbase = createClient(supabaseUrl, supabaseKey);
 
 type UserType = string | null;
 type ProjectType = any;
@@ -35,8 +45,82 @@ export default function Page({ params }: { params: { slug: string } }) {
       const { data, error } = await supabase.auth.getSession();
       if (data) {
         console.log(data, data?.session?.user);
+        console.log("here", user);
+        if (data?.session?.user) {
+          setUser(data?.session?.user?.id);
+          console.log("here now", user);
+          try {
+            let { data: projects, error } = await superbase
+              .from('projects')
+              .select('*')
+              .eq('id', params.slug);
+    
+            if (error) {
+              setError(error.message);
+            } else {
+              setProjects(projects || []);
+              setLat(projects?.[0].lattitude)
+              setLong(projects?.[0].longitude)
+              console.log(projects?.[0].project_name)
+              console.log(projects?.[0].lattitude)
+              console.log(projects?.[0].longitude)
+              const apiUrl = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${projects?.[0].lattitude}&lon=${projects?.[0].longitude}&date=${previousDayDate}&appid=${process.env.NEXT_PUBLIC_OPENWEATHERMAP}`;
+              console.log(lat, long)
+          // Make the API call using the fetch API
+              fetch(apiUrl)
+                .then((response) => response.json())
+                .then((data) => {
+                  setWeatherData(data);
+                  console.log(data)
+                })
+                .catch((error) => {
+                  console.error('Error fetching weather data:', error);
+                });
+
+            }
+          } catch (error: any) {
+            setError(error.message);
+          }
+        }
+      } else {
+        console.log('false ities');
+      }
+    };
+    afunctiontocall();
+  }, []);
+  
+  
+  return (
+    <>
+      <div>My Post: {params.slug}</div>
+      <div>Yesterdays Date: {previousDayDate}</div>
+      <div>Yesterdays weather: {JSON.stringify(weatherData)}</div>
+      <div>My Id: {user}</div>
+    </>
+  );
+}
+
+//pull user.id
+//make sure it matches the uuid before rendering
+//pull in project data
+//pull in rain and weather data
+//http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+//
+//create dashboard
+//create google map
+//edit information form button
+//reporting status
+
+
+  /*useEffect(() => {
+    const afunctiontocall = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data) {
+        console.log(data, data?.session?.user);
+        console.log("here", user)
         if (data?.session?.user) {
           setUser(data?.session?.user?.id)
+          console.log("here now", user)
           //setLat()
           //setLong()
         }
@@ -45,21 +129,40 @@ export default function Page({ params }: { params: { slug: string } }) {
       }
     };
     afunctiontocall();
-  }, []);
-
-  useEffect(() => {
-    async function fetchProjects() {
+    const bfunctiontocall = async function fetchProjects() {
       try {
         let { data: projects, error } = await supabase
-          .from('projects')
-          .select('*')
-          .eq('id', params.slug);
+          .from('projects').select('*')
+          .eq('id',params.slug);
 
         if (error) {
           setError(error.message);
         } else {
-          setProjects(projects || [] || null);
+          setProjects(projects||[]);
           console.log(projects);
+          console.log(params.slug)
+          console.log(error);
+        }
+      } catch (error: any) {
+        setError(error.message);
+      }
+    }
+    bfunctiontocall();
+  }, []);*/
+ /*
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        let { data: projects, error } = await supabase
+          .from('projects').select('*')
+          .eq('id',params.slug);
+
+        if (error) {
+          setError(error.message);
+        } else {
+          setProjects(projects||[]);
+          console.log(projects);
+          console.log(params.slug)
           console.log(error);
         }
       } catch (error: any) {
@@ -68,7 +171,7 @@ export default function Page({ params }: { params: { slug: string } }) {
     }
     console.log(projects, 'data displayed here');
     fetchProjects();
-  }, []);
+  }, [JSON.stringify(user)]);*/
 
 /*
   useEffect(() => {
@@ -85,24 +188,3 @@ export default function Page({ params }: { params: { slug: string } }) {
         console.error('Error fetching weather data:', error);
       });
   }, [long]); */
-
-  return (
-    <>
-      <div>My Post: {params.slug}</div>
-      <div>Yesterdays Date: {previousDayDate}</div>
-      <div>Yesterdays weather: {previousDayDate}</div>
-      <div>My Id: {user}</div>
-    </>
-  );
-}
-
-//pull user.id
-//make sure it matches the uuid before rendering
-//pull in project data
-//pull in rain and weather data
-//http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
-//
-//create dashboard
-//create google map
-//edit information form button
-//reporting status
